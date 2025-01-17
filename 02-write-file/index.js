c73203d3a0ccc83a8588b6a1620690246b2e78b6
ŅatalyKisialiova <nataly.kisialiova@gmail.com>
2025-01-18 01:21:25 +0200

    feat: add resolving 02-write-file task

---
+++ 02-write-file/index.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const { stdin, stdout } = process;
+const path = require('path');
+const filePath = path.join(__dirname, 'text.txt');
+const output = fs.createWriteStream(filePath);
+
+stdout.write('Hello! Enter the text.\n');
+
+stdin.on('data', (data) => {
+  if (data.toString().trim().toLowerCase() === 'exit') {
+    cleanAndExit();
+    // console.log('Good luck learning Node.js!');
+  } else {
+    output.write(data);
+  }
+});
+
+process.on('SIGINT', cleanAndExit);
+
+function cleanAndExit() {
+  output.end(() => {
+    fs.unlink(filePath, (err) => {
+      if (err) {
+        console.error('Ошибка при удалении файла:', err);
+      }
+      console.log('Good luck learning Node.js!');
+      process.exit();
+    });
+  });
+}
