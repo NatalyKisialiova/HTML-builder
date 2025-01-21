167b0d19abaad8adab793241dea5cc0084d74ec8
ŅatalyKisialiova <nataly.kisialiova@gmail.com>
2025-01-21 02:13:11 +0200

    feat: add creation index.html file, copy and replace template tags from template.html file in the 06-build-page task

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,21 +1,23 @@
-const fs = require('fs/promises');
+const fs = require('fs');
+const fsPromise = require('fs/promises');
 const path = require('path');
 
-fs.mkdir(path.join(__dirname, 'proect-dist'), { recursive: true });
-// const indexPath = path.join(__dirname, 'proect-dist', 'index.html');
+fsPromise.mkdir(path.join(__dirname, 'proect-dist'), { recursive: true });
+const indexPath = path.join(__dirname, 'proect-dist', 'index.html');
 // const output = require('fs').createWriteStream(indexPath);
 const stylePath = path.join(__dirname, 'proect-dist', 'style.css');
 const assetsCopyPath = path.join(__dirname, 'proect-dist');
-fs.mkdir(path.join(assetsCopyPath, 'assets'));
+fsPromise.mkdir(path.join(assetsCopyPath, 'assets'));
 
-fs.readdir(path.join(__dirname, 'styles'), { withFileTypes: true })
+fsPromise
+  .readdir(path.join(__dirname, 'styles'), { withFileTypes: true })
   .then((files) => {
-    const output = require('fs').createWriteStream(stylePath);
+    const output = fs.createWriteStream(stylePath);
     files.forEach((element) => {
       const extension = path.extname(element.name);
       if (element.isFile() && extension === '.css') {
         const filePath = path.join(__dirname, 'styles', element.name);
-        const readStream = require('fs').createReadStream(filePath, 'utf8');
+        const readStream = fs.createReadStream(filePath, 'utf8');
         readStream.on('data', (chunk) => {
           output.write(chunk);
         });
@@ -25,44 +27,47 @@ fs.readdir(path.join(__dirname, 'styles'), { withFileTypes: true })
   .catch((error) => {
     console.error('Error', error);
   });
-// const templatePath = path.join(__dirname, 'template.html');
-// fs.copyFile(templatePath, indexPath);
-// const readStreamTemplate = require('fs').createReadStream(templatePath, 'utf8');
+const templatePath = path.join(__dirname, 'template.html');
+const readStreamTemplate = fs.createReadStream(templatePath, 'utf8');
 
-// readStreamTemplate.on('data', (chunk) => {
-//   let templateContent = chunk;
+readStreamTemplate.on('data', (chunk) => {
+  let templateContent = chunk;
 
-//   fs.readdir(path.join(__dirname, 'components'), { withFileTypes: true }).then(
-//     (files) => {
-//       files.forEach((file) => {
-//         if (file.isFile() && path.extname(file.name) === '.html') {
-//           const fileComponentName = path.basename(file.name);
-//           const fileComponentPath = path.extname(
-//             path.join(__dirname, 'components', file.name),
-//           );
-//           const fileComponentPath2 = path.join(
-//             __dirname,
-//             'components',
-//             file.name,
-//           );
-//           console.log(fileComponentName, fileComponentPath, fileComponentPath2);
-//           const fileComponentContent = require('fs').createReadStream(
-//             fileComponentPath2,
-//             'utf8',
-//           );
-//           fileComponentContent.on('data', (data) => {
-//             let result = data;
-//             // console.log(templateContent);
-//             templateContent = templateContent.replace(
-//               `{{${file.name.replace(fileComponentPath, '')}}}`,
-//               result,
-//             );
-//             console.log(file.name.replace(fileComponentPath, ''));
-//             console.log(result);
-//             output.write(templateContent);
-//           });
-//         }
-//       });
-//     },
-//   );
-// });
+  fsPromise
+    .readdir(path.join(__dirname, 'components'), { withFileTypes: true })
+    .then((files) => {
+      files.forEach((file) => {
+        if (file.isFile() && path.extname(file.name) === '.html') {
+          const fileComponentName = path.basename(file.name);
+          const fileExtension = path.extname(
+            path.join(__dirname, 'components', file.name),
+          );
+          const fileComponentPath = path.join(
+            __dirname,
+            'components',
+            file.name,
+          );
+          console.log(fileComponentName);
+          const fileComponentContent = fs.createReadStream(
+            fileComponentPath,
+            'utf8',
+          );
+          fileComponentContent.on('data', (data) => {
+            let result = data;
+            // console.log(templateContent);
+            templateContent = templateContent.replace(
+              `{{${file.name.replace(fileExtension, '')}}}`,
+              result,
+            );
+            // console.log(result);
+            // output.write(templateContent);
+          });
+          fileComponentContent.on('data', () => {
+            fs.writeFile(indexPath, templateContent, 'utf8', (error) => {
+              if (error) throw error;
+            });
+          });
+        }
+      });
+    });
+});
